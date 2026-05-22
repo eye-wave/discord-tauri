23c4cf88d7c43ae9387242b96ffc3cf9841b64fb
eyewave <eyewave@noreply.codeberg.org>
2026-05-22 15:16:25 +0200

    update build script

--- build.rs
+++ build.rs
@@ -6,6 +6,25 @@ fn main() {
     let _ = bundle_js();
 }
 
+#[derive(PartialEq)]
+enum TargetOs {
+    Macos,
+    Linux,
+    Window,
+    Unknown,
+}
+
+impl From<&str> for TargetOs {
+    fn from(value: &str) -> Self {
+        match value {
+            "MACOS" => Self::Macos,
+            "LINUX" => Self::Linux,
+            "WINDOWS" => Self::Window,
+            _ => Self::Unknown,
+        }
+    }
+}
+
 fn bundle_js() -> io::Result<()> {
     println!("cargo:rerun-if-changed=js/");
 
@@ -14,13 +33,22 @@ fn bundle_js() -> io::Result<()> {
         .into();
     let js_dir = manifest_dir.join("js");
 
+    let target = if cfg!(target_os = "macos") {
+        TargetOs::Macos
+    } else if cfg!(target_os = "windows") {
+        TargetOs::Window
+    } else if cfg!(target_os = "linux") {
+        TargetOs::Linux
+    } else {
+        TargetOs::Unknown
+    };
+
     let code = fs::read_dir(js_dir)?
         .filter_map(Result::ok)
         .filter_map(|f| {
             let content = fs::read_to_string(f.path()).ok()?;
-            let is_macos = cfg!(target_os = "macos");
 
-            if content.starts_with("// #TARGET MACOS") && !is_macos {
+            if is_target(&target, &content) {
                 None
             } else {
                 Some(content)
@@ -29,18 +57,32 @@ fn bundle_js() -> io::Result<()> {
         .intersperse("\n".to_string())
         .collect::<String>();
 
-    let out_path = manifest_dir.join("target/scripts.js");
+    let out_path = manifest_dir.join("target").join("scripts.js");
 
     fs::write(out_path, minify_js(&code))?;
 
     Ok(())
 }
 
+fn is_target(target: &TargetOs, code: &str) -> bool {
+    const PRAGMA: &str = "// #TARGET ";
+
+    let start = PRAGMA.len();
+    let src_os: TargetOs = code[start..start + 7]
+        .find('\n')
+        .map(|i| &code[start..start + i])
+        .unwrap_or_default()
+        .into();
+
+    *target == src_os
+}
+
 fn minify_js(code: &str) -> String {
     use std::sync::Arc;
     use swc::BoolOrDataConfig;
-    use swc::config::JsMinifyOptions;
+    use swc::config::{IsModule, JsMinifyOptions};
     use swc_common::{FileName, GLOBALS, Globals, SourceMap};
+    use swc_ecma_minifier::option::terser::{TerserCompressorOptions, TerserTopLevelOptions};
 
     let cm = Arc::new(SourceMap::default());
     let compiler = swc::Compiler::new(cm.clone());
@@ -53,8 +95,16 @@ fn minify_js(code: &str) -> String {
                 fm,
                 handler,
                 &JsMinifyOptions {
-                    compress: BoolOrDataConfig::from_bool(true),
-                    mangle: BoolOrDataConfig::from_bool(true),
+                    compress: BoolOrDataConfig::from_obj(TerserCompressorOptions {
+                        toplevel: Some(TerserTopLevelOptions::Bool(false)),
+                        top_retain: None,
+                        ..Default::default()
+                    }),
+                    mangle: BoolOrDataConfig::from_obj(swc_ecma_minifier::option::MangleOptions {
+                        top_level: Some(false),
+                        ..Default::default()
+                    }),
+                    module: IsModule::Bool(false),
                     ..Default::default()
                 },
                 Default::default(),
